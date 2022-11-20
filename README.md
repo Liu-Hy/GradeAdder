06c8a77d9cb4bd3950f2e56da54ee505a9160c71
techt <hl57@illinois.edu>
2022-11-20 09:47:45 -0600

    implemented

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # GradeAdder
+Examples:
+1.
+![img_1.png](img_1.png) \
+add_grades(25, ec=1, q_num=14) \
+get result 21.62 
+2.
+![img_2.png](img_2.png) \
+add_grades(7, ec=0, index=[1,'2a','2b', 3]) \
+ger result 6.25 
+3.
+![img_3.png](img_3.png) \
+add_grades(6, ec=2, index=['Part1-1', '2', 'Part2', "Part3", "ec"]) \
+get result 5.0
--- run.py
+++ run.py
@@ -7,7 +7,6 @@ def read_input() -> List[str]:
     """
     print("Please paste the grading comment:")
     lines = []
-    new_line = None
     while True:
         new_line = str(input())
         if new_line == "":
@@ -18,63 +17,75 @@ def read_input() -> List[str]:
             lines.append(new_line)
 
 
-def add_up(full_mark: float, ec=0, q_num=None, index=None) -> float:
+def add_grades(full_mark: float, ec=0, q_num=None, index=None, neglect_str="*"*3, digits=2) -> float:
     """ Read the list of string, repeatedly prompt the user until the input is correct, and compute the total grade of
     the student.
-    :param q_num: the number of questions in the exam, including the extra credit(EC) question if applicable
-    :param full_mark: the full mark of the exam excluding the EC
-    :param ec: the full score of the ec question. 0 means there is no ec question in the exam.
-    :param index: the user specified list of question identifiers in the exam. When provided, it overrides the index automatically generated according to q_num and has_ec
+    :param full_mark: the full mark of the exam excluding the extra credit (EC)
+    :param ec: the full score of the EC question. ec = 0 means there is no EC question.
+    :param q_num: the number of questions in the exam, including the EC question if applicable. Together with ec, they
+    are used by the program to generate a question index against which the input string is checked
+    :param index: the user-specified list of question identifiers in the exam. When provided, it overrides the
+    automatically generated index. And if ec > 0, the last item of the index must be "ec".
     :return: a float number of the student's grade.
     """
-    assert not (q_num is None and index is None)
-    assert isinstance(full_mark, (float, int)) and full_mark > 0, f"invalid full mark {full_mark}"
-    assert isinstance(ec, (float, int)) and ec >= 0, f"invalid ec {ec}"
+    PE = "Parameter error: "
+    IE = "Input error: "
+    assert isinstance(full_mark, (float, int)) and full_mark > 0, PE + f"invalid full mark {full_mark}!"
+    assert isinstance(ec, (float, int)) and ec >= 0, PE + f"invalid ec {ec}!"
     if q_num is not None:
-        assert isinstance(q_num, int) and q_num > 0, f"invalid q_num {q_num}"
+        assert isinstance(q_num, int) and q_num > 0, PE + f"invalid q_num {q_num}!"
     elif index is not None:
-        assert isinstance(index, list) and len(index) > 0, f"invalid index {index}"
+        assert isinstance(index, list) and len(index) > 0, PE + f"invalid index {index}!"
     else:
-        print("q_num and index cannot both be None")
+        print(PE + "one of q_num and index must be provided!")
         return
 
-    # If the exam has an extra credict question, and the user specifies a question index, "ec" must be the last item of the index
     full_mark = float(full_mark)
     ec = float(ec)
     if index is not None:
-        index = [str(i) for i in index]
+        index = [str(i).strip() for i in index]
         if "ec" in index and ec == 0:
-            print("ec is listed in the index but is worth 0 points")
+            print(PE + "ec is listed in the index but is worth 0 points")
         if "ec" not in index and ec > 0:
-            print("ec is not listed in the index but is worth some points")
+            print(PE + "ec is not listed in the index but is worth some points")
+
     while True:
         lines = read_input()
         act_idx = []
+        # Initialize total actual score and total full score
         tot_act_sc, tot_full_sc = 0., 0.
         has_error = False
         for k, line in enumerate(lines):
+            # If the line is a divider for different parts of the exam, skip it
+            if line.startswith(neglect_str) or line.endswith(neglect_str):
+                continue
             fields = line.split(":", 1)  # only split on the first colon if any
             if len(fields) < 2:
-                print(f"Input error: the {k}th row should contain a ':' seperator.")
+                print(IE + f"the {k}th row should contain a ':' seperator.")
                 has_error = True
                 break
             idx, rest = fields[0].strip(), fields[1].strip()
             act_idx.append(idx)
             scores = rest.split("/", 1)  # only split on the first slash if any
             if len(scores) < 2:
-                print(f"For question {idx}, the line should contain a '/' seperator.")
+                print(IE + f"For question {idx}, the line should contain a '/' seperator.")
                 has_error = True
                 break
+            # If there is a bracketed comment at the end of the line, ignore it
             if "(" in scores[1]:
-                scores[1] = scores[1].split("(", 1)[0]  # neglect the annotation if any
+                scores[1] = scores[1].split("(", 1)[0]
             try:
                 act_sc, full_sc = float(scores[0].strip()), float(scores[1].strip())
             except ValueError as e:
-                print(f"{e} for question {idx}")
+                print(IE + f"{e} for question {idx}")
+                has_error = True
+                break
+            if full_sc < 0:
+                print(IE + f"invalid full score for question {idx}. ")
                 has_error = True
                 break
             if act_sc < 0 or act_sc > full_sc:
-                print(f"Invalid actual score for question {idx}. ")
+                print(IE + f"invalid actual score for question {idx}. ")
                 has_error = True
                 break
             # If no errors are found in the question line itself, accumulate the actual score and full score
@@ -86,48 +97,52 @@ def add_up(full_mark: float, ec=0, q_num=None, index=None) -> float:
                 if act_idx == index:
                     if tot_full_sc != (full_mark + ec):
                         print(
-                            f"The full mark of the exam is {full_mark + ec} whereas the full points of questions add up to {tot_full_sc}")
-                        continue
+                            IE + f"The full mark of the exam is {full_mark + ec} whereas the full points of questions add up to {tot_full_sc}")
+                        continue  # continue to prompt user for input
                 elif act_idx == index[:-1]:
-                    if index[-1] == "ec":
+                    if index[-1] == "ec":  # We support the omission of last ec question row because most students don't do it
                         if tot_full_sc != full_mark:
                             print(
-                                "The full mark of the exam excluding the EC is {full_mark}, whereas the full points of the questions except EC add up to {tot_full_sc}")
+                                IE + "The full mark of the exam excluding the EC is {full_mark}, whereas the full points of the questions except EC add up to {tot_full_sc}")
                             continue
                     else:
-                        print("The last item of the specified index is missing in the string, and it is not the 'ec'")
+                        print(IE + "The last item of the specified index is missing from the string, and it is not the 'ec'")
                 else:
-                    print("The index in the string should match the specified index")
+                    print(IE + "The index in the string should match the specified index")
             else:
+                # automatically generate question index
                 index = [str(i) for i in range(1, q_num + 1)]
                 if ec == 0:
                     if act_idx != index:
-                        print(f"Index in the string should follow the format: ['1', '2', ..., '{q_num}']")
+                        print(IE + f"Index in the string should follow the format: ['1', '2', ..., '{q_num}']")
                         continue
                     if tot_full_sc != full_mark:
                         print(
-                            f"The full mark of the exam is {full_mark} whereas the full points of questions add up to {tot_full_sc}")
+                            IE + f"The full mark of the exam is {full_mark} whereas the full points of questions add up to {tot_full_sc}")
                         continue
                 else:
                     index[-1] = "ec"
                     if act_idx == index:
                         if tot_full_sc != (full_mark + ec):
                             print(
-                                f"The full mark of the exam including the ec is {full_mark + ec} whereas the full points of questions add up to {tot_full_sc}")
+                                IE + f"The full mark of the exam including the ec is {full_mark + ec} whereas the full points of questions add up to {tot_full_sc}")
                             continue
                     elif act_idx == index[:-1]:
                         if tot_full_sc != full_mark:
                             print(
-                                f"The full mark of the exam excluding the EC is {full_mark}, whereas the full points of the questions except EC add up to {tot_full_sc}")
+                                IE + f"The full mark of the exam excluding the EC is {full_mark}, whereas the full points of the questions except EC add up to {tot_full_sc}")
                             continue
                     else:
                         print(
-                            f"Index in the string should follow the format: ['1', '2', ..., '{q_num - 1}', 'ec']. Only the last item could be omitted.")
+                            IE + f"Index in the string should follow the format: ['1', '2', ..., '{q_num - 1}', 'ec'], where only the last item could be omitted.")
                         continue
-
+            if digits > 0:
+                tot_act_sc = round(tot_act_sc, digits)
             print(f"The student's total grade: {tot_act_sc}\n")
 
 
 if __name__ == '__main__':
-    add_up(25, ec=1, q_num=14)
+    #add_grades(25, ec=1, q_num=14)
     #add_up(25, ec=1, index=[1, 2, 3, 4, "5a", "5b", "5c", 6, 7, 8, 9, 10, 11, "ec"])
+    #add_grades(7, ec=0, index=[1,'2a','2b', 3])
+    add_grades(6, ec=2, index=['Part1-1', '2', 'Part2', "Part3", "ec"])
